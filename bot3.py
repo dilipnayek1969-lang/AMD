4a6fb43c4cd8082727bc02d47bc757b1e6dc0433
dilipnayek1969-lang <dilipnayek1969@gmail.com>
2025-12-28 22:29:02 +0530

    Create bot3.py

---
+++ bot3.py
@@ -0,0 +1,11 @@
+i make a telegram bot by python , Name Auto-Rename-Bot , the bot rename file by it's given formate by user and add metadata and thumbnail and meta data which is given before by user then rename and set the video by it's given media formate (File format and Video formate) and then send rename video to the user , user set Auto rename formate , metadata , thumbnail, media formate , before upload or forwarding the files and these are aplicable for all files. when it generate video formate then the frame size and ratio but be same as thumbnail. I have the bot but the bot can't add metadata , I want to solve it. And the bot can't generate in video formate . I give you the bot code and .env file . .env : ADMIN=1869817167
+API_HASH=217e861ebca9da0dd4c17b1abf92636c
+API_ID=25775944
+BOT_TOKEN=7750507797:AAFT5QgxqdKnqDBu_2ZkjFxo9u5fBNOF5qY
+CHANNEL_ID=-1002795055491
+DB_NAME=Filex
+DB_URL="mongodb+srv://Filex:Guddu8972771037@cluster0.er3kfsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DUMP_CHANNEL=0
+FORCE_SUB_CHANNELS=-1002795055491
+LOG_CHANNEL=-1002795055491
+WEBHOOK=False  bot code:
